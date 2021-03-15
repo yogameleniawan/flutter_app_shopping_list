122b79f8e0e6230c70f874c339a3bfb4e34d735c
yogameleniawan <64576201+yogameleniawan@users.noreply.github.com>
2021-03-15 14:58:36 +0700

    Praktikum 4 - Pembacaan Data

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:pemrograman_mobile_week6/models/item.dart';
 
 class ItemPage extends StatelessWidget {
   const ItemPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Item itemArgs = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
         title: Text("Shopping List"),
+      ),
+      body: Center(
+        child: Text(itemArgs.name + " with " + itemArgs.price.toString()),
       ),
     );
   }
