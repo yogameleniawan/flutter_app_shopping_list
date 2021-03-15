95e045b9a6aaf1917824ad0fdfdcd84dcb07d96c
yogameleniawan <64576201+yogameleniawan@users.noreply.github.com>
2021-03-15 14:33:30 +0700

    Praktikum 1 - Mendefinisikan Route

---
+++ lib/pages/item_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItemPage extends StatelessWidget {
+  const ItemPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
